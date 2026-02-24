cf5bb67da6af13bc62a393ccbca6592c6b8bda42
leoBello <leobello.wd@gmail.com>
2026-02-24 08:40:53 +0100

    add mock user

---
+++ src/hooks/tests/__mock__/mockedGithubUser.ts
@@ -0,0 +1,64 @@
+import type { GithubUser } from '../../../services/githubApi';
+
+export const mockGithubUsers: GithubUser[] = [
+  {
+    id: 1,
+    login: 'octocat',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/octocat',
+  },
+  {
+    id: 2,
+    login: 'torvalds',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1024025?v=4',
+    html_url: 'https://github.com/torvalds',
+  },
+  {
+    id: 3,
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+    html_url: 'https://github.com/mojombo',
+  },
+  {
+    id: 4,
+    login: 'defunkt',
+    avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
+    html_url: 'https://github.com/defunkt',
+  },
+  {
+    id: 5,
+    login: 'tj',
+    avatar_url: 'https://avatars.githubusercontent.com/u/49933?v=4',
+    html_url: 'https://github.com/tj',
+  },
+  {
+    id: 6,
+    login: 'jeresig',
+    avatar_url: 'https://avatars.githubusercontent.com/u/12?v=4',
+    html_url: 'https://github.com/jeresig',
+  },
+  {
+    id: 7,
+    login: 'gaearon',
+    avatar_url: 'https://avatars.githubusercontent.com/u/810438?v=4',
+    html_url: 'https://github.com/gaearon',
+  },
+  {
+    id: 8,
+    login: 'paulirish',
+    avatar_url: 'https://avatars.githubusercontent.com/u/39191?v=4',
+    html_url: 'https://github.com/paulirish',
+  },
+  {
+    id: 9,
+    login: 'addyosmani',
+    avatar_url: 'https://avatars.githubusercontent.com/u/150330?v=4',
+    html_url: 'https://github.com/addyosmani',
+  },
+  {
+    id: 10,
+    login: 'dtolnay',
+    avatar_url: 'https://avatars.githubusercontent.com/u/310448?v=4',
+    html_url: 'https://github.com/dtolnay',
+  },
+];
